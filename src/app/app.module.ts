1e57539e36beb8eb9bd5c973577b1d3e7b1df86d
cobbie <evelinagonda@gmail.com>
2017-04-11 21:14:08 +0200

    promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import { HeroesComponent } from './heroes.component';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
   declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
